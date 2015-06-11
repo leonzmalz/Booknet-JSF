af663924046fd23a9b45f987d4475e0ab7a7c145
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2015-06-11 19:01:44 -0300

    mod CadastrarValores

--- src/main/java/br/com/booknet/controle/CadastrarValoresBean.java
+++ src/main/java/br/com/booknet/controle/CadastrarValoresBean.java
@@ -14,22 +14,47 @@ import br.com.booknet.repositorio.RepositorioDeLivros;
 @ManagedBean
 @ViewScoped
 public class CadastrarValoresBean implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Valores valores;
 	private List<Livro> listaDeLivros;
 	private RepositorioDeLivros livros;
-	
+
 	public CadastrarValoresBean() {
 		// TODO Auto-generated constructor stub
 		this.valores = new Valores();
-		this.livros  = new RepositorioDeLivros();
+		this.livros = new RepositorioDeLivros();
 	}
-	
-	public void inicializar(){
+
+	public void inicializar() {
 		this.listaDeLivros = livros.todos();
 	}
-	
+
+	public Valores getValores() {
+		return valores;
+	}
+
+	public void setValores(Valores valores) {
+		this.valores = valores;
+	}
+
+	public List<Livro> getListaDeLivros() {
+		return listaDeLivros;
+	}
+
+	public void setListaDeLivros(List<Livro> listaDeLivros) {
+		this.listaDeLivros = listaDeLivros;
+	}
+
+	public void salvar() {
+		/*produtos.guardar(produto, categoria);
+
+		this.produto = new Produto();
+
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage("Produto " + operacao + " com sucesso")); */
+
+	}
 
 }
